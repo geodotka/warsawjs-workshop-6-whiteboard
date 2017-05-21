6b8ca7a6b7ecd1d69aa409f5a33d9be73ae8a6ba
geodotka <dorota.kowalik@21s.pl>
2017-05-21 16:37:23 +0200

    Add object to canvas

--- client/templates/Board/Boards.js
+++ client/templates/Board/Boards.js
@@ -3,6 +3,8 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Tracker } from 'meteor/tracker';
 import { Session } from 'meteor/session';
+import { Random } from 'meteor/random';
+import Objects from '/imports/collections/Objects';
 
 Template.Board.onCreated(function() {
     this.boardId = new ReactiveVar();
@@ -42,7 +44,72 @@ Template.Board.onRendered(function() {
     this.autorun(() => {
         canvas.isDrawingMode = Session.get('isDrawingMode');
         canvas.freeDrawingBrush.color = Session.get('color');
-    })
+    });
+
+    canvas.on('object:added', (e) => {
+        const fabricObject = e.target;
+        if (fabricObject.id) {
+            // Object was added by someone else and the "object:added" event was
+            // called or we just refreshed page and objects are being added to the
+            // canvas. In both situations we just stop here to not allow reinserting
+            // an object.
+            return;
+        }
+        // Convert fabric object to JSON.
+        const object = fabricObject.toObject();
+        // We have to generate id by ourself because we can't use id returned by
+        // Object.insert. Before Object.insert returns the "added" event is already
+        // called and we need to set id on the fabricObject before that.
+        object._id = fabricObject.id = Random.id();
+        object.boardId = fabricObject.boardId = this.boardId.get();
+        fabricObject.userId = Meteor.userId();
+        Meteor.call('insertObject', object, (err) => {
+            if (err) {
+            alert(err.message);
+            canvas.remove(fabricObject);
+            }
+        });
+        console.log('canvas:object:added', fabricObject.id);
+    });
+
+    this.autorun(() => {
+        Objects.find().observeChanges({
+            added(id, doc) {
+            console.log('observeChanges.added', id, doc);
+            const fabricObject = canvas.getObjectById(id);
+            if (fabricObject) {
+                // Object is already drawn on canvas so we stop here to not draw
+                // duplicate.
+                return;
+            }
+            // Create fabric objects from JSON.
+            fabric.util.enlivenObjects([doc], ([fabricObject]) => {
+                fabricObject.id = id;
+                canvas.add(fabricObject);
+            });
+            },
+            changed(id, fields) {
+            console.log('observeChanges.changed', id, fields);
+            const fabricObject = canvas.getObjectById(id);
+            if (!fabricObject) {
+                // We can't update object that does not exist on the canvas.
+                return;
+            }
+            fabricObject.set(fields);
+            canvas.renderAll();
+            },
+            removed(id) {
+            console.log('observeChanges.removed', id);
+            const fabricObject = canvas.getObjectById(id);
+            if (!fabricObject) {
+                // Object was already removed or does not exist so we have nothing
+                // to do here.
+                return;
+            }
+            canvas.remove(fabricObject);
+            }
+        });
+    });
 });
 
 
