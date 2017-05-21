ef1e035199fb8c4c6c00942126b4d77f0e66a5b3
geodotka <dorota.kowalik@21s.pl>
2017-05-21 12:11:45 +0200

    Move method to both.js file

--- server/main.js
+++ server/main.js
@@ -3,12 +3,3 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
-
-
-// meteor zaleca używać coś na wzór restowego api
-Meteor.methods({
-    // możęmy stworzyć metody, które będą odpowiadać konkretnym operacjom
-    createPost(doc) {
-        Posts.insert(doc);
-    }
-});
