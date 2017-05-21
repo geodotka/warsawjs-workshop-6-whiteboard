5736a5d81ca9767b783f11a071ac66ca626c67c5
geodotka <dorota.kowalik@21s.pl>
2017-05-21 15:47:20 +0200

    Draw on canvas

--- client/templates/Board/Boards.js
+++ client/templates/Board/Boards.js
@@ -1,6 +1,8 @@
+import { fabric } from 'fabric';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Tracker } from 'meteor/tracker';
+import { Session } from 'meteor/session';
 
 Template.Board.onCreated(function() {
     this.boardId = new ReactiveVar();
@@ -13,4 +15,37 @@ Template.Board.onCreated(function() {
         this.subscribe('boardById', boardId);   // subskrybcje są reaktywne automatycznie
         this.subscribe('objectsByBoardId', boardId);
     });
+});
+
+
+Template.Board.onRendered(function() {
+    // canvas już istnieje
+    const canvasElement = this.find('canvas');
+
+    // create new fabric canvas
+    const canvas = this.canvas = new fabric.Canvas(canvasElement, {
+        // disable group selection
+        selection: false
+    });
+
+    // nie możemy zrobić tego stylami
+    const resizeCanvas = function() {
+        canvas.setHeight(window.innerHeight);
+        canvas.setWidth(window.innerWidth);
+        canvas.renderAll();
+       };
+    resizeCanvas();
+    this.resizeHandler = window.addEventListener('resize', () => {
+        resizeCanvas();
+    });
+
+    this.autorun(() => {
+        canvas.isDrawingMode = Session.get('isDrawingMode');
+        canvas.freeDrawingBrush.color = Session.get('color');
+    })
+});
+
+
+Template.Board.onDestroyed(function() {
+   window.removeEventListener('resize', this.resizeHandler)
 });
