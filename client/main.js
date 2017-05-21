b19528889fbebb320d85de8f9c1289940fe349cf
geodotka <dorota.kowalik@21s.pl>
2017-05-21 11:13:01 +0200

    Add second button

--- client/main.js
+++ client/main.js
@@ -5,7 +5,7 @@ import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
-  this.counter = new ReactiveVar(0);
+  this.counter = new ReactiveVar(0);    // wartość reaktywna, ustawiamy ją 'set', wyciągamy 'get'
 });
 
 Template.hello.helpers({
@@ -15,8 +15,12 @@ Template.hello.helpers({
 });
 
 Template.hello.events({
-  'click button'(event, instance) {
+  'click .create'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
+  },
+  'click .remove'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() - 1);
   },
 });
