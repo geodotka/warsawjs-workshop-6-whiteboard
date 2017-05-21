17ac1c96ffa121db1aa983ea31c41b2e0c33ae33
geodotka <dorota.kowalik@21s.pl>
2017-05-21 11:57:03 +0200

    Set permissions

--- server/main.js
+++ server/main.js
@@ -3,3 +3,18 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+
+Posts.allow({
+    insert(userId, doc) {
+//        return true;     // umożliwiamy wszystkim inserty
+        var user = Users.findOne(userId);
+        if (userId) {
+            return true;
+        }
+    },
+    update(){
+
+    }
+});
+
